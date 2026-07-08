9b5e678ad6a0d7b072e5da0ed4b69830020c009d
Sara-Rubaya <sararubaya4800@gmail.com>
2026-07-09 00:12:21 +0600

    Setting Up Route, Controller, Service For User Registration API

---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,11 @@
+import { Request, Response, Router } from "express";
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
+import config from "../../config";
+import httpStatus  from "http-status";
+import { userController } from "./user.controller";
+
+const router = Router();
+// ---Register user---
+router.post("/register" ,userController.registerUser )
+ export const userRoutes = router;
