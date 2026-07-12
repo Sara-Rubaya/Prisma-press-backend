555c88b47914a4c7ff893649a27f498a2c39ed0c
Sara-Rubaya <sararubaya4800@gmail.com>
2026-07-12 20:52:12 +0600

    added comment & post

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,36 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import express, { Application, Request, Response } from "express";
 import config from "./config";
-import httpStatus  from "http-status";
-import { prisma } from "./lib/prisma";
-import bcrypt from "bcryptjs";
-import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.routes";
 
-
-
+import { userRoutes } from "./modules/user/user.route";
+import { postRoutes } from "./modules/post/post.routes";
+import { commentRoutes } from "./modules/comment/comment.routes";
 
 
 
 const app : Application = express();
+
 app.use(cors({
     origin : config.app_url,
     credentials : true,
 }))
+
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended : true }));
 app.use(cookieParser());
 
-app.get("/", (req : Request,res : Response)=>{
-    res.send("Hello World!");
+
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
 });
 
-// ---Register user---
 // app.post()
+
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
 
 export default app;
